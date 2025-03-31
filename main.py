57ba35d2ede76a8c18010a1ae01e44ceac74c7b1
F-Franke <f.franke@edu.pucrs.br>
2025-03-31 11:15:09 -0300

    adicionamos os metodos de sobrecarga

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ class Produto:
         if novo_preco > 0:
             self.preco = novo_preco
     
-    def aplicar_desconto(self,percentual):
-        preco_descontado = preco*percentual/100
-        preco_com_desconto = preco - preco_descontado
+    def aplicar_desconto(self,percentual,preco):
+        preco_descontado = self.preco*percentual/100
+        preco_com_desconto = self.preco - preco_descontado
         self.preco =  preco_com_desconto
     
-    def verificar_estoque_baixo():
+    def verificar_estoque_baixo(self,estoque):
         if self.estoque < 5:
             return True
         else:
@@ -36,11 +36,9 @@ class Cliente:
     def resgatar_pontos(self,pontos):
         if pontos > 0:
             self.pontos_fidelidade =- pontos
-    def verificar_vip():
-        if self.pontos_fidelidade >= 1000:
-            return True
-        else:
-            return False
+
+    def verificar_vip(self,pontos_fidelidade):
+        return self.pontos_fidelidade >= 1000
         
     def __str__(self):
         return f"Cliente: {self.nome} | Pontos: {self.pontos_fidelidade}"
@@ -51,5 +49,23 @@ class Cliente:
     def __iadd__(self,pontos):
         if pontos > 0:
             self.pontos_fidelidade =+ pontos
+
+class pedido:
+
+    def __init__(self,cliente,produto,quantidade,status):
+        self.cliente = Cliente
+        self.produto = Produto
+        self.quantidade = quantidade
+        self.status = status
+        
+    def calcular_total(self):
+        return self.produto.preco * self.pedido.peso_total
+        
+    def calcular_peso_total(self):
+        peso_total = self.produto.peso_kg * self.quantidade
+        return peso_total
+    
+    def atualizar_status(status_novo):
+
     
     
