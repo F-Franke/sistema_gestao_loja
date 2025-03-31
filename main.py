cfe2b86ca9f9a962fc52fedfed25f9380fdda07c
F-Franke <f.franke@edu.pucrs.br>
2025-03-31 10:57:17 -0300

    criamos classe Cliente

--- main.py
+++ main.py
@@ -13,12 +13,43 @@ class Produto:
         preco_com_desconto = preco - preco_descontado
         self.preco =  preco_com_desconto
     
-    def verificar_estoque_baixo(self,estoque):
-        estoque_baixo = False
-        if estoque < 5:
-            estoque_baixo = True
+    def verificar_estoque_baixo():
+        if self.estoque < 5:
+            return True
+        else:
+            return False
+        
     def __str__(self):
         return f"Produto: {self.nome} | Preco: {self.preco}"
     def __eq__(self, other):
-        return self.nome == other.nome and self.preco == othr.preco
+        return self.nome == other.nome and self.preco == other.preco
+
+class Cliente:
+    def __init__(self,nome,email,pontos_fidelidade):
+        self.nome = nome
+        self.email = email
+        self.pontos_fidelidade = pontos_fidelidade
+
+    def adicionar_pontos(self,pontos):
+        if pontos > 0:
+            self.pontos_fidelidade =+ pontos
+    def resgatar_pontos(self,pontos):
+        if pontos > 0:
+            self.pontos_fidelidade =- pontos
+    def verificar_vip():
+        if self.pontos_fidelidade >= 1000:
+            return True
+        else:
+            return False
         
+    def __str__(self):
+        return f"Cliente: {self.nome} | Pontos: {self.pontos_fidelidade}"
+    
+    def __eq__(self,other):
+        return self.email == other.email
+    
+    def __iadd__(self,pontos):
+        if pontos > 0:
+            self.pontos_fidelidade =+ pontos
+    
+    
